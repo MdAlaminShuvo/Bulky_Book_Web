ecf035c0b0dfd5e4a3009cf850bdb4897a7e9c7b
Md Al Amin <alamin.cse.ru2018@gmail.com>
2024-03-22 15:04:25 +0600

    Product model are created

--- BulkyBook.Models/Prodect.cs
+++ BulkyBook.Models/Prodect.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,7 +33,15 @@ namespace BulkyBook.Models
         [Range(1, 10000)]
         public double Price100 { get; set; }
 
-        public string ImageUrl { get; set; } 
+        public string ImageUrl { get; set; }
 
+        [Required]
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+
+        [Required]
+        public int CoverTypeId { get; set; }
+        public CoverType CoverType { get; set; }
     }
 }
