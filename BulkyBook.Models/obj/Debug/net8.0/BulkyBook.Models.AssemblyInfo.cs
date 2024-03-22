9f313b80dc334747e74dfda6ceac6f5e8151a2a7
Md Al Amin <alamin.cse.ru2018@gmail.com>
2024-03-22 14:58:35 +0600

    Product model are created

--- BulkyBook.Models/obj/Debug/net8.0/BulkyBook.Models.AssemblyInfo.cs
+++ BulkyBook.Models/obj/Debug/net8.0/BulkyBook.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyBook.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45d0d79b901a160f29532a350a09670df002b883")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02da94eddf53946e61bbb0e59207d182b3a58dfb")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyBook.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyBook.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
